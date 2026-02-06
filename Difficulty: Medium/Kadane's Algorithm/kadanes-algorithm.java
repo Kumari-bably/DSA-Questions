f9fd7a39324feff45c5ae2fabcc1eb122c81fb1f
Kumari Bably <kumari7488bably@gmail.com>
2026-02-07 02:26:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -10,16 +10,7 @@ class Solution {
 
         return maxSum;
     }
+}
 
     // For testing
-    public static void main(String[] args) {
-        int[] arr1 = {2, 3, -8, 7, -1, 2, 3};
-        System.out.println(maxSubarraySum(arr1)); // 11
-
-        int[] arr2 = {-2, -4};
-        System.out.println(maxSubarraySum(arr2)); // -2
-
-        int[] arr3 = {5, 4, 1, 7, 8};
-        System.out.println(maxSubarraySum(arr3)); // 25
-    }
-}
+   
