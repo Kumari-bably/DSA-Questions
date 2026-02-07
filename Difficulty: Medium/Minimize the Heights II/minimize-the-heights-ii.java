37f6834fa4dcc295c052e0b3ecc00201fb35cb55
Kumari Bably <kumari7488bably@gmail.com>
2026-02-07 11:40:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    int getMinDiff(int[] arr, int k) {
+
+        int n = arr.length;
+        if (n == 1) return 0;
+
+        Arrays.sort(arr);
+
+        int ans = arr[n - 1] - arr[0];
+
+        int smallest = arr[0] + k;
+        int largest = arr[n - 1] - k;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            int minVal = Math.min(smallest, arr[i + 1] - k);
+            int maxVal = Math.max(largest, arr[i] + k);
+
+            if (minVal < 0) continue;
+
+            ans = Math.min(ans, maxVal - minVal);
+        }
+
+        return ans;
+    }
+}
