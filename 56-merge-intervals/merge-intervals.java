a460d605b4a1801806a206221176d0c22e57f9c1
Kumari Bably <kumari7488bably@gmail.com>
2026-02-07 11:47:43 +0530

    Time: 8 ms (90.14%) | Memory: 49.2 MB (53.35%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        // Sort by start time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> merged = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            // Fixed typo here: use 'interval' not 'interva'
+            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < interval[0]) {
+                merged.add(interval);
+            } else {
+                // There is overlap, so merge
+                merged.get(merged.size() - 1)[1] =
+                    Math.max(merged.get(merged.size() - 1)[1], interval[1]);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
